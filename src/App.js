3e30567f0f5db1a61ccb957400a55b04b1cd4cf6
MiguelAngelAlvarezFernandez <mayprofprog@gmail.com>
2023-03-03 12:53:39 +0100

    Prueba calculo precio por persona sin useEfect

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [Total, setTotal] = useState(0);
   const [Persoas, setPersoas] = useState(1);
 
-  const [PagoPersoa, setPagoPersoa] = useState(0);
+  /*const [PagoPersoa, setPagoPersoa] = useState(0);*/
 
   function manexadorTotal (event){
     setTotal(event.target.value)
@@ -18,7 +18,7 @@ function App() {
     setPersoas(event.target.value)
   }
 
-  useEffect(
+  /*useEffect(
   function calcPagoPersoa () {
     let dividendo = Total
     let divisor = Persoas
@@ -26,7 +26,12 @@ function App() {
     setPagoPersoa(calculo)
   },
   [Total, Persoas]
-  )
+  )*/
+
+  function calcPagoPersoa(dividendo, divisor){
+    let calculo = (dividendo/divisor)
+    return calculo
+  }
 
   return (
     <>
@@ -41,7 +46,9 @@ function App() {
       {Persoas}
       </div>
 
-      <p>{PagoPersoa}</p>
+      <p>Con useEffect {/*{PagoPersoa}*/}</p>
+
+      <p>Solo con función calcPagoPersoa: {calcPagoPersoa(Total, Persoas)}</p>
 
       <Saldo/>
 
