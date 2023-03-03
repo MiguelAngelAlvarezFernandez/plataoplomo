187aaf6822b17a842d477ac258136bd261dbff89
Juan José Hidalgo Neira <tempocompartido@hotmail.com>
2023-03-03 17:17:42 +0100

    El .... input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
 
 
@@ -7,20 +7,14 @@ function App() {
 
   const [Total, setTotal] = useState(0);
   const [Persoas, setPersoas] = useState(1);
-  const [Pago,setPago] = useState(1);
-
+  
   function manexadorTotal (event){
     setTotal(event.target.value)
-    /*setPago(Total/Persoas)*/ 
   }
 
   function manexadorPersoas (event){
     setPersoas(event.target.value)
-    /*setPago(Total/Persoas)*/
   }
-
-  useEffect(() => {Total/Persoas}, [Total, Persoas]);
-
    return (
     <>
       <label for="total">Total a pagar:</label>
@@ -30,11 +24,10 @@ function App() {
       </div>
 
       <label for="pax"> Nº de Persoas:</label>
-      <input type="range" id="pax" name="pax" min="1" max="20" onInput={manexadorPersoas}/>
+      <input type="range" id="pax" name="pax" min="1" max="20" value={Persoas} onInput={manexadorPersoas}/>
       <div>
       {Persoas}
       </div>
-      {(Pago)}
 
     </>
   );
